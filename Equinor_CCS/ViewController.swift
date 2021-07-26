d8c3104b3913fa1e9d8157f5f40901a43bb9d8da
marielaulie <54407324+marielaulie@users.noreply.github.com>
2021-07-26 12:36:50 +0200

    Opprettet app

---
+++ Equinor_CCS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Equinor_CCS
+//
+//  Created by Mariel Aulie Hinderaker on 26/07/2021.
+//  Copyright © 2021 Mariel Aulie Hinderaker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
